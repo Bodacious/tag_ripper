23b25c2c88ee03d269ff86f5aac29b36596df898
Gavin Morrice <gavin@gavinmorrice.com>
2025-03-25 17:32:18 +0000

    Move Ripper class into TagRipper module

--- lib/tag_ripper.rb
+++ lib/tag_ripper.rb
@@ -2,6 +2,45 @@
 
 require "ripper"
 
+module TagRipper
+  class Ripper
+    def initialize(file_path)
+      @tokens = ::Ripper.lex(File.read(file_path))
+      @stored_taggables = []
+    end
+
+    def taggables
+      @taggables ||= process_taggables
+    end
+
+    protected
+
+    def process_taggables # rubocop:disable Metrics
+      @tokens.each do |(col, line), type, token, _|
+        lex = Lex.new([col, line], type, token)
+        next if lex.ignored?
+
+        @current_taggable ||= Taggable.new
+        @current_taggable.add_tag(lex.tag_name, lex.tag_value) if lex.tag_comment?
+        if lex.taggable_definition? && @current_taggable.named?
+          @current_taggable = Taggable.new(parent: @current_taggable)
+        elsif lex.taggable_definition?
+          @current_taggable.open
+        end
+
+        @current_taggable.name_from_lex(lex) if lex.taggable_name?
+
+        next unless lex.end?
+        raise "Can't close nil scope" unless @current_taggable
+
+        @current_taggable.end!
+        @stored_taggables << @current_taggable
+        @current_taggable = @current_taggable.parent
+      end
+      @stored_taggables
+    end
+  end
+end
 class Taggable
   def initialize(name: nil, parent: nil)
     @name = name
@@ -162,40 +201,3 @@ class Lex
   end
 end
 
-class TagRipper
-  def initialize(file_path)
-    @tokens = Ripper.lex(File.read(file_path))
-    @stored_taggables = []
-  end
-
-  def taggables
-    @taggables ||= process_taggables
-  end
-
-  protected
-
-  def process_taggables # rubocop:disable Metrics
-    @tokens.each do |(col, line), type, token, _|
-      lex = Lex.new([col, line], type, token)
-      next if lex.ignored?
-
-      @current_taggable ||= Taggable.new
-      @current_taggable.add_tag(lex.tag_name, lex.tag_value) if lex.tag_comment?
-      if lex.taggable_definition? && @current_taggable.named?
-        @current_taggable = Taggable.new(parent: @current_taggable)
-      elsif lex.taggable_definition?
-        @current_taggable.open
-      end
-
-      @current_taggable.name_from_lex(lex) if lex.taggable_name?
-
-      next unless lex.end?
-      raise "Can't close nil scope" unless @current_taggable
-
-      @current_taggable.end!
-      @stored_taggables << @current_taggable
-      @current_taggable = @current_taggable.parent
-    end
-    @stored_taggables
-  end
-end
--- test/tag_ripper_test.rb
+++ test/tag_ripper_test.rb
@@ -5,13 +5,13 @@ require "tag_ripper"
 class TagRipperTest < Minitest::Test
   using CustomAssertions
   def test_returns_a_list_of_taggables
-    tag_ripper = TagRipper.new(Tempfile.new)
+    tag_ripper = TagRipper::Ripper.new(Tempfile.new)
 
     assert_empty tag_ripper.taggables
   end
 
   def test_detects_tag_comment_on_module
-    tag_ripper = TagRipper.new("./test/fixtures/simple_example.rb")
+    tag_ripper = TagRipper::Ripper.new("./test/fixtures/simple_example.rb")
 
     taggable = tag_ripper.taggables.find { |t| t.name == "Foo" }
 
