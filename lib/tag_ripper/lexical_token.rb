75007e802551e6bd842379c9348415da39aff7fc
Gavin Morrice <gavin@gavinmorrice.com>
2025-03-29 10:21:26 +0000

    Remove unused methods from the Lex class

--- lib/tag_ripper/lexical_token.rb
+++ lib/tag_ripper/lexical_token.rb
@@ -1,20 +1,5 @@
 module TagRipper
   class LexicalToken
-    IGNORED_TYPES = %i[on_sp
-                       on_tstring_beg
-                       on_tstring_content
-                       on_tstring_end
-                       on_nl
-                       on_ignored_nl
-                       on_period].freeze
-
-    IGNORED_TYPES_AND_TOKENS = Hash.new do |hash, key|
-      hash[key] = []
-    end
-    IGNORED_TYPES_AND_TOKENS[:on_ident] << "private"
-    IGNORED_TYPES_AND_TOKENS[:on_ident] << "require"
-    NAME_IDENTIFIERS = %i[on_const on_ident].freeze
-
     class Location
       attr_reader :col
       attr_reader :line
@@ -51,16 +36,8 @@ module TagRipper
       type == :on_comment
     end
 
-    def keyword?
-      type == :on_kw
-    end
-
     def tag_comment?
       comment? && token.match?(/# @domain: (.+)/)
-    end
-
-    def non_tag_comment?
-      comment? && !tag_comment?
     end
 
     def tag_name
@@ -72,20 +49,6 @@ module TagRipper
     end
 
     alias tag_value tag_values
-
-    # def ignore?
-    #   IGNORED_TYPES.include?(type) ||
-    #     IGNORED_TYPES_AND_TOKENS[type].include?(token) ||
-    #     non_tag_comment?
-    # end
-
-    def taggable_definition?
-      keyword? && token.match(/class|module|def/)
-    end
-
-    def taggable_name?
-      NAME_IDENTIFIERS.include?(type)
-    end
 
     def end?
       keyword? && token.match(/end/)
