127428b157bb2b0a295c529a20fa2d89953f8f0b
Gavin Morrice <gavin@gavinmorrice.com>
2025-03-29 23:28:53 +0000

    Load the Set class explicitly if not defined

--- lib/tag_ripper.rb
+++ lib/tag_ripper.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
+require 'set' unless defined?(Set)
 require "ripper"
 require_relative "tag_ripper/lexical_token"
 require_relative "tag_ripper/taggable_entity"
 require_relative "tag_ripper/configuration"
-
 module TagRipper
   class << self
     def configure(&)
