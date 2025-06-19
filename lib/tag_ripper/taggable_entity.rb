bcf1851e1cf514ae7b15800e95e8e88645e32e81
Gavin Morrice <gavin@gavinmorrice.com>
2025-06-19 22:53:10 +0100

    Auto-correct various Rubocop complaints

--- lib/tag_ripper/taggable_entity.rb
+++ lib/tag_ripper/taggable_entity.rb
@@ -34,8 +34,6 @@ module TagRipper
     # @return [Array<Symbol>]
     OPENED_STATUSES = %i[tagged awaiting_name named].freeze
 
-
-    #
     def initialize(name: nil, parent: nil)
       @name = name
       @tags = Hash.new { |hash, key| hash[key] = Set.new }
@@ -163,7 +161,6 @@ module TagRipper
       @name.concat(string.to_s)
     end
 
-
     def status=(status)
       status = status.to_sym
       raise InvalidStatusError unless VALID_STATUSES.include?(status)
@@ -228,24 +225,25 @@ module TagRipper
     alias on_kw_module on_new_taggable_context_kw
     alias on_kw_class on_new_taggable_context_kw
 
-    IGNORED_IDENT_KEYWORDS = %w[require private class_eval instance_eval define_method].freeze
+    IGNORED_IDENT_KEYWORDS = %w[require private class_eval instance_eval
+                                define_method].freeze
     private_constant :IGNORED_IDENT_KEYWORDS
 
-    def name_from_lex(lex)
+    def name_from_lex(lex) # rubocop:disable Metrics
       return self if IGNORED_IDENT_KEYWORDS.include?(lex.token)
       # TODO: Simplify this logic
-      return self if named? && !@name.end_with?('::')
-      return self unless may_name? || @name.end_with?('::')
+      return self if named? && !@name.end_with?("::")
+      return self unless may_name? || @name.end_with?("::")
 
       # self.status = :awaiting_name # TODO: Fix this with a proper state
-      if named? && @name.end_with?('::')
+      if named? && @name.end_with?("::")
         append_name!(lex.token)
       else
         self.name = "#{name}#{lex.token}"
       end
 
       self
-    end
+    end # rubocop:enable Metrics
 
     ##
     # Matches names of constants: module names, const names, etc.
